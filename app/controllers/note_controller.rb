55316eaaf751d7a898fe49f12f10dd16daf443ee
Katrina Tran <kat.c.tran@gmail.com>
2018-08-31 19:08:16 -0600

    added some crud

--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -1,14 +1,18 @@
 class NoteController < ApplicationController
   def index
+    @note = Note.all
   end
 
   def show
+    @note = Note.find(params[:id])
   end
 
   def edit
+    @note = Note.find(params[:id])
   end
 
   def new
+    @note = Note.new
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'note/index'
-  get 'note/show'
-  get 'note/edit'
-  get 'note/new'
-  get 'note/update'
-  get 'note/create'
-  get 'note/destroy'
+  resources :note
+  root 'note#index'
+  # get 'note/index'
+  # get 'note/show'
+  # get 'note/edit'
+  # get 'note/new'
+  # get 'note/update'
+  # get 'note/create'
+  # get 'note/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
